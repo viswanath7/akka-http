ae948bbf71a02e0c1fa39ed832abe4142d5b587a
Jayachandran, Viswanath <Viswanath.Jayachandran@aegon.nl>
2017-11-14 09:00:37 +0100

    Adaption for reactive web client

--- src/main/scala/com/example/akka/application/client/RequestLevelClient.scala
+++ src/main/scala/com/example/akka/application/client/RequestLevelClient.scala
@@ -21,6 +21,16 @@ object RequestLevelClient extends App with JsonSupport {
 	
 	val ipAddressAPIURL = "https://api.ipify.org?format=json"
 	
+	/**
+		* Akka HTTP is streaming all the way through, which means that back-pressure mechanisms enabled by Akka Streams are
+		* exposed through all layers–from TCP layer, through HTTP server to user-facing HttpRequest and HttpResponse
+		* and their HttpEntity APIs. It means, lack of consumption of the HTTP Entity, is signaled as back-pressure to
+		* the other side of the connection.
+		*
+		* Consuming (or discarding) the Entity of a request is mandatory! If accidentally left neither consumed or discarded
+		* Akka HTTP will assume the incoming data should remain back-pressured, and will stall the incoming data via
+		* TCP back-pressure mechanisms. A client should consume the Entity regardless of the status of the HttpResponse.
+		*/
 	Http()
 		.singleRequest(HttpRequest(uri = ipAddressAPIURL))
 		.onComplete {
@@ -37,7 +47,14 @@ object RequestLevelClient extends App with JsonSupport {
 			case Success(internetProtocolAddress) => logger info s"Your IP address is ${internetProtocolAddress.ip}"
 			case Failure(error) => logger error("Error encountered while unmarshalling response entity", error)
 		}
-		case statusCode => logger error s"Failed to retrieve ip address. HTTP response status code : $statusCode"
+		case statusCode =>
+			logger error s"Failed to retrieve ip address. HTTP response status code : $statusCode"
+			/**
+				* We do not care about the response payload but the entity still has to be consumed otherwise,
+				* one would exert back-pressure on underlying TCP client.
+				* The following method pipes incoming bytes directly to Sink.ignore
+				*/
+			httpResponse.discardEntityBytes.future.onComplete{ _ => logger debug "HTTP response entity discarded!"}
 	}
 	
 	
